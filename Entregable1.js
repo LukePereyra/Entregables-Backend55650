353dddfda5c58c6dc5a19031061e2ba8455ae97e
LukePereyra <lucaspereyra2019@gmail.com>
2023-11-19 12:36:58 -0300

    entregable1

--- Entregable1.js
+++ Entregable1.js
@@ -1,6 +1,50 @@
 class ProductManager {
-    constructor (products) [] {
-        
-        
+    constructor() {
+        this.products = []
+    }
+
+    addProduct(title, description, price, thumbnail, code, stock) {
+
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            console.error("Hay que completar todos los campos")
+        }
+        if (this.products.some(product => product.code === code)) {
+            console.error("Ya hay un producto con ese code")
+        }
+        const product = {
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        }
+        this.products.push(product)
+    }
+
+    getProducts() {
+        return this.products;
+    }
+
+    getProductById(id) {
+        const product = this.products.find(product = product.id === id)
+        if (product) {
+            return product;
+        } else {
+            console.error("Producto con id:", id, "no encontrado")
+            return null;
+        }
     }
 }
+
+const manager = new ProductManager()
+
+manager.addProduct("Milanesa", "de Soja", 2000, "https://i.ibb.co/VT9N9wN/Milanesa-Soja.jpg", "01", 24)
+manager.addProduct("Albóndiga", "de Lentejas", 1800, "https://i.ibb.co/GHYbvTR/Albondigas.jpg", "02", 12)
+//manager.addProduct("Tarta", "",2500, "https://i.ibb.co/2d5sD3f/Tarta-Acelga.jpg", "03", 4)
+manager.addProduct("Pizza", "Vegana", 4000, "https://imgs.search.brave.com/siJH1qUyw-koncJWz9guBi6pYzLTaeE0PwbXKuf8puc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/aW5mb2JhZS5jb20v/bmV3LXJlc2l6ZXIv/YlJpbVdWeFU5TGpN/Sk8tQ2pxYjJyRWV2/VVVZPS9maWx0ZXJz/OmZvcm1hdCh3ZWJw/KTpxdWFsaXR5KDg1/KS9hcmMtYW5nbGVy/ZmlzaC1hcmMyLXBy/b2QtaW5mb2JhZS5z/My5hbWF6b25hd3Mu/Y29tL3B1YmxpYy9D/T08zSFE0MzJCRUVK/R1BYNEc3TkdDRE9W/NC5qcGc","04", 6)
+
+
+//console.log(manager.getProducts())
+
+console.log(manager.getProductById(1))
